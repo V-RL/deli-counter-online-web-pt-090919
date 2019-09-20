867dc394ae044e14232c57761832f97964458032
Brandon Furr <brandon.furr123@gmail.com>
2019-09-20 13:14:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+def line(katz_deli)
+  current_line = "The line is currently:" 
+  if katz_deli.empty? 
+    puts "The line is currently empty."
+  else 
+    katz_deli.each.with_index(1) do |peeps, y|
+      current_line << " #{y}. #{peeps}"
+    end
+    puts current_line
+  end
+end
+
+def take_a_number(katz_deli, name)
+  if katz_deli.length >= 0
+    katz_deli.push"#{name}"
+end
+puts "Welcome"
+end
